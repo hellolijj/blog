9ee77b5dc78258d943ae4ec748af1c45af7b05c7
Junjun Li <junjunli666@gmail.com>
2018-10-08 11:39:59 +0800

    rebuilding site 2018年10月 8日 星期一 11时39分54秒 CST

--- deploy.sh
+++ deploy.sh
@@ -5,20 +5,28 @@ set -eux
 echo -e "\033[0;32mDeploying updates to GitHub...\033[0m"
 
 msg="rebuilding site `date`"
-
 if [ $# -eq 1  ]
     then msg="$1"
 fi
 
 # Build the project. 
-hugo -d docs
+hugo # if using a theme, replace by `hugo -t <yourtheme>`
+
+# Go To Public folder
+cd public
 
 # Add changes to git.
 git add .
 
 # Commit changes.
-
 git commit -m "$msg"
 
 # Push source and build repos.
+git push origin master
+
+# Come Back
+cd ..
+
+git add .
+git commit -m "$msg"
 git push origin master
